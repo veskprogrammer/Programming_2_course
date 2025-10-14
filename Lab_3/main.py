b53fd1a5e2ad84c80b23661fa850d1c7c00db162
Киселев Георгий <kendstudio.beats@yandex.ru>
2025-10-14 10:20:31 +0300

    Update main.py

--- Lab_3/main.py
+++ Lab_3/main.py
@@ -1,10 +1,20 @@
-def gen_bin_tree(height=5, root=6):
+from typing import Callable
+def gen_bin_tree(height = 5, root = 6) -> dict: 
+    
+    """Генерация бинарного дерева в виде вложенных словарей.
+
+    params:
+        height - высота дерева
+        root - значение корня
+        left_leaf - функция для вычисления значения левого листа
+        right_leaf - функция для вычисления значения правого листа
+        """ 
+    left_leaf = (root * 2) - 2
+    right_leaf = root + 4
+
     if height == 0:
         return None
-    left = (root * 2) - 2
-    right = root + 4
-    return {
-        'value': root,
-        'left': gen_bin_tree(height - 1, left),
-        'right': gen_bin_tree(height - 1, right)
-    }
+    
+    return {'value': root,
+            'left': gen_bin_tree(height-1, left_leaf),
+            'right': gen_bin_tree(height-1, right_leaf)}
