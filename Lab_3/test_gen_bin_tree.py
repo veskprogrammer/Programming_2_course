ed3baf4a18acca33b0ba3e40f34cdd8bc3e65c5d
Киселев Георгий <kendstudio.beats@yandex.ru>
2025-10-14 10:20:54 +0300

    Update test_gen_bin_tree.py

--- Lab_3/test_gen_bin_tree.py
+++ Lab_3/test_gen_bin_tree.py
@@ -2,26 +2,17 @@ import unittest
 from main import gen_bin_tree
 
 class TestGenBinTree(unittest.TestCase):
-    def test_default_tree(self):
-        tree = gen_bin_tree()
-        self.assertIsInstance(tree, dict)
-        self.assertEqual(tree['value'], 6)
-        self.assertIn('left', tree)
-        self.assertIn('right', tree)
+    def test_height_0(self):
+        self.assertEqual(gen_bin_tree(0), None)
 
-    def test_height_zero(self):
-        tree = gen_bin_tree(height=0, root=10)
-        self.assertIsNone(tree)
+    def test_height_1(self):
+        self.assertEqual(gen_bin_tree(1), {'value': 6, 'left': None, 'right': None})
 
-    def test_custom_root_and_height(self):
-        tree = gen_bin_tree(height=2, root=3)
-        self.assertEqual(tree['value'], 3)
-        self.assertEqual(tree['left']['value'], 4)
-        self.assertEqual(tree['right']['value'], 7)
-        self.assertIsNone(tree['left']['left'])
-        self.assertIsNone(tree['left']['right'])
-        self.assertIsNone(tree['right']['left'])
-        self.assertIsNone(tree['right']['right'])
-
+    def test_height_2(self):
+        self.assertEqual(gen_bin_tree(2), 
+                         {'value': 6, 
+                          'left': {'value': 10, 'left': None, 'right': None}, 
+                          'right': {'value': 10, 'left': None, 'right': None}})
+        
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
